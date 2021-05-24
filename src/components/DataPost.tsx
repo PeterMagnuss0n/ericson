709216083e36b5eebd4368489d566fca88aecd61
PeterMagnuss0n <peter.magnusson@hiq.se>
2021-05-25 01:55:10 +0200

    css changes

--- src/components/DataPost.tsx
+++ src/components/DataPost.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import '../App.css';
+import '../styles/post.css';
+
 
 function DataFetch() {
     const [title, setTitle] = useState<any>("");
@@ -7,27 +8,29 @@ function DataFetch() {
   
     const onTitleChange = (e: { target: { value: any; }; }) => setTitle(e.target.value);
     const onBodyChange = (e: { target: { value: any; }; }) => setBody(e.target.value);
-  
+    
     const handlePost = (e: { preventDefault: () => void; }) => {
-      e.preventDefault();
-  
-      const data = { title, body };
-      const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-      };
+        e.preventDefault();
 
-      fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
-        .then(response => response.json())
-        .then(res => console.log(res));
+        const data = { title, body };
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        };
+
+        fetch("https://jsonplaceholder.typicode.com/posts", requestOptions)
+            .then(response => response.json())
+            .then(res => console.log(res));
     };
     return (
-        <div>
+        <div className="post-holder">
             <form>
-                <input placeholder="Titel" value={title} onChange={onTitleChange} required />
-                <textarea placeholder="Text" value={body} onChange={onBodyChange} required />            
-                <button type="submit" onClick={handlePost}>Skapa inlägg</button>
+                <div className="post-fields">
+                    <input className="input-field fields" placeholder="Titel" value={title} onChange={onTitleChange} required />
+                    <textarea className="fields post-text" placeholder="Text" value={body} onChange={onBodyChange} required />            
+                    <button className="post-button fields" type="submit" onClick={handlePost}>Skapa inlägg</button>
+                </div>
             </form>
         </div>
     )
