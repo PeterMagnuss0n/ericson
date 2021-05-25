5887f6b2c0a715def3a05f835f5a4df6c32e1239
PeterMagnuss0n <peter.magnusson@hiq.se>
2021-05-25 02:29:37 +0200

    Added gallery

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import DataFetch from './components/DataFetch';
 import DataPost from './components/DataPost';
+import Images from './components/Images';
 
 function App() {
   return (
     <div className="App">
         <DataFetch></DataFetch>
         <DataPost></DataPost>
+        <Images></Images>
     </div>
   );
 }
---
+++ src/components/Images.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import '../styles/images.css'
+
+function Images() {
+    return (
+    <div className="image-holder">
+        <div className="flex">
+            <div className="image-content">
+                <img src="/images/desert_sand_night_138892_3840x2160.jpg" alt="" />
+                <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+            </div>
+            <div className="image-content">
+                <img src="/images/navagio_bay_greece-wallpaper-1920x1080.jpg" alt="" />
+                <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+            </div>
+            <div className="image-content">
+                <img src="/images/one_tree_one_cloud-wallpaper-1920x1080.jpg" alt="" />
+                <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+            </div>
+            <div className="image-content">
+                <img src="/images/road_lighting_night_119175_3840x2160.jpg" alt="" />
+                <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+            </div>
+            <div className="image-content">
+                <img src="/images/view_of_ocean_from_the_rock-wallpaper-2560x1440.jpg" alt="" />
+                <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+            </div>
+            <div className="image-content">
+                <img src="/images/photo-of-cliff-during-daytime-3259814.jpg" alt="" />
+                <div>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</div>
+            </div>
+        </div>
+    </div>   
+    )
+}
+
+export default Images
